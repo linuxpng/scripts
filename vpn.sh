4298db3735ae019468044e67b9f9518eca61f40c
Steve Barcomb <steve@jazzpuppy.org>
2020-05-17 12:32:33 -0600

    Add a script to make a resilient vpn connection

---
+++ vpn.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create a TOTP token and read the secret using a QR code scanner, this will be <yoursecret>
+# You can optionally stick this in cron or wrap it in a 'while do' loop if you want to keep it running in a tmux session like I do
+
+token=$(oathtool --base32 --totp <yoursecret>)
+pin=<yourpin>
+user=<yourusername>
+homedir="<directory with configuration files>"
+vpnup=$(ip addr | grep tun0 | grep inet | awk '{print $2}')
+config=<nameofconf>
+
+if [ -z "$vpnup" ]
+then
+	echo $user > $homedir/pass.txt
+	echo $pin$token >> $homedir/pass.txt
+	sudo openvpn --client --config $homedir/$confg --auth-user-pass $homedir/pass.txt --setenv PATH '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' --script-security 2 --up /etc/openvpn/update-resolv-conf --down /etc/openvpn/update-resolv-conf --down-pre
+else
+	:
+fi
+
+sleep 60
